ea938f66f0688884a4049d766f43cf86fc4c26de
Howard <howardl93@yahoo.com>
2020-06-23 14:59:08 -0700

    adding actions

--- src/actions/data_actions.js
+++ src/actions/data_actions.js
@@ -1,11 +1,23 @@
 
 // area_actions.js
 
+
+
 import * as APIUtil from '../util/api_util';
 
 export const RECEIVE_AREA = 'RECEIVE_AREA';
 
 export const RECEIVE_DATE = 'RECEIVE_DATE';
+
+export const RECEIVE_ALL = 'RECEIVE_ALL';
+
+export const RECEIVE_FILTERED = 'RECEIVE_FILTERED';
+
+
+const receiveFiltered = filtered = ({
+	type: RECEIVE_FILTERED,
+	filtered
+})
 
 
 const receiveDate = date =>({
@@ -17,8 +29,17 @@ const receiveDate = date =>({
 const receiveState = area => ({
 	type: RECEIVE_AREA,
 	area
-})
+});
 
-const fetchCoronaData = async (area, date)=>{
-	await APIUtil.fetchAreaData(area);
+
+const receiveAll = all => ({
+	type: RECEIVE_ALL,
+	all
+
+});
+
+
+export const fetchGeneralData = () =>  dispatch => {
+	return APIUtil.fetchGeneralData()
+	.then(res => dispatch(receiveAll(res)))
 }
