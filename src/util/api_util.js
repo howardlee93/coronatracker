6f5a5ab8cff340231ee50f3a14a1fc4d41524e34
Howard <howardl93@yahoo.com>
2020-07-02 12:27:43 -0700

    cleaning up util

--- src/util/api_util.js
+++ src/util/api_util.js
@@ -5,36 +5,21 @@ import axios from 'axios';
 const baseURL = 'https://covidtracking.com/api/v1/states/';
 
 
-// https://covidtracking.com/api/v1/states/
-
 export const fetchHistoricAreaData = (area, date) => {
 	return axios.get(baseURL + `${area}/${date}.json`)
-	// .then(response => console.log(response))
-	// .catch(err => console.log(err));
 
 };
-
 
 
 export const fetchDateData = date => {
 	return axios.get(baseURL +`us/${date}.json`)
-	// .then(res => console.log(res))
-	// .catch(err => console.log(err));
+	
 	
 }
 
 
-export const fetchGeneralData = () => { // not sure if this is needed
-	return axios.get(baseURL +'us/current.json')
-	// .then(response => console.log(response))
-	// .catch(err => console.log(err));
-
-};
-
 export const fetchAreaData = area => {
 	return axios.get(baseURL +`${area}/current.json`)
-	// .then(res => console.log(res))
-	// .catch(err => console.log(err));
 	
 }
 
